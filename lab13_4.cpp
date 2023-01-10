f842b9390d7fbf3ae0ccce4eeab42ad1db7828ea
MeThussss <mathatt16@gmail.com>
2023-01-10 16:02:07 +0700

    complete

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
-
 using namespace std;
+
+template <typename Type>
+void mySwap(Type &,Type &); 
 
 int main(){
 	int x, y;
@@ -30,3 +32,9 @@ int main(){
 	return 0;
 }
 
+template <typename Type>
+void mySwap(Type &x,Type &y){
+	Type a = x;
+	x = y;
+	y = a;
+}
